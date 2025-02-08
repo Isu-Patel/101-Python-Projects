bbdd38078fdc4b711358e66ab62f670f63f784fd
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-08 22:57:28 +0530

    Update Surface_Area_and_Volume_of_Sphere.py

--- Basic/Projects/Surface_Area_and_Volume_of_Sphere.py
+++ Basic/Projects/Surface_Area_and_Volume_of_Sphere.py
@@ -1,1 +1,11 @@
+import math
+# Get input from the user for radius
+radius = float(input("Enter the radius of the sphere: "))
 
+# Calculate the volume and surface area of the sphere
+surface_area = 4 * math.pi * radius ** 2
+volume = (4/3) * math.pi * radius ** 3
+
+# Displaying the results as per given radius
+print(f"Surface Area of the Sphere is {surface_area:.2f}")
+print(f"Volume of the Sphere is {volume:.2f}")
