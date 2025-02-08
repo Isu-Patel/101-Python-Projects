e0b39e65c8d7f758f6d878979f75b89d9504ce1a
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-08 20:56:01 +0530

    Update loops.py

--- Loops/loops.py
+++ Loops/loops.py
@@ -22,6 +22,23 @@ for i in numbers:
     
 print(f"Total sum is {sum}")
 
-
+print("\nFunction 2 : For Loop with range function")
 for num in range(10):  # 0 to 9
     print(num)
+
+print("\nFunction 3 : For Loop with range and using start to stop function")
+for i in range(12, 59, 4):
+    print(f"The Numbers are: {i}")
+
+
+# ---------------------- Program 2 : While Loop ---------------------- #
+print("\n---------------------- Program 2 : While Loop ----------------------")
+
+a = 10
+sum = 0
+i = 1
+
+while i <= a:
+    sum = sum + i
+    i = i + 1
+    print(f"Sum after adding {i} is {sum}")
