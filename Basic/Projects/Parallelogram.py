7459ec9ce5685eab492be9f1540e094d17d8dd61
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-08 22:39:59 +0530

    Create Parallelogram.py

---
+++ Basic/Projects/Parallelogram.py
@@ -0,0 +1,9 @@
+# Asking for base and radius to find out its area
+base = float(input("Enter the base of the parallelogram: "))
+height = float(input("Enter the height of the parallelogram: "))
+
+# Executing Formula of finding area of parallelogram which is base X Height or b X h
+area = base * height
+
+# Telling what is the area of the base and height given by the user.
+print(f"The area of the parallelogram is {area}")
