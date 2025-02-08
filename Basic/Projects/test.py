945767c703618759c517d51f0ae8c1622e45e483
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-08 22:22:10 +0530

    Create test.py

---
+++ Basic/Projects/test.py
@@ -0,0 +1,15 @@
+# Basic "Hello World! program"
+from numpy import intp
+
+
+print("Hello World!")
+
+# Asking for User's name and granting him hello
+name = input("What is your name? ")
+print(f"Hello {name}! Welcome to Python!")
+
+
+# Asking for User's favorite color and granting him the same
+color = input("What is your favorite color? ")
+print(f"Wow! {color} is a beautiful color!")
+
