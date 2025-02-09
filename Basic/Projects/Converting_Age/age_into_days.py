5a746ff6a13bd6a8e97775f9c1b526296b0cf6f8
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 22:15:31 +0530

    Create age_into_days.py

---
+++ Basic/Projects/Converting_Age/age_into_days.py
@@ -0,0 +1,9 @@
+age_year = int(input("Enter your age in years: "))
+
+days = age_year * 365
+
+print(f"You have lived for approximately {days} days.")
+
+# ---------------------------- Output ---------------------------- #
+# Enter your age in years: 25
+# You have lived for approximately 9125 days.
