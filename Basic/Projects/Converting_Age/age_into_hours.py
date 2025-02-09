0a900ccec4fc1bd2569139cf1bfcf760b37a092e
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 22:10:59 +0530

    Create age_into_hours.py

---
+++ Basic/Projects/Converting_Age/age_into_hours.py
@@ -0,0 +1,9 @@
+age_years = int(input("Enter your age in years: "))
+
+hours = age_years * 365 * 24
+
+print(f"You have lived for approximately {hours} hours.")
+
+# ---------------------------- Output ---------------------------- #
+# Enter your age in years: 25
+# You have lived for approximately 219000 hours.
