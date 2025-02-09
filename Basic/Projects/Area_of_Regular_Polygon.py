2a6d40c8cb4a5b7faee785e3fe8549f5bdba881c
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 21:38:17 +0530

    Create Area_of_Regular_Polygon.py

---
+++ Basic/Projects/Area_of_Regular_Polygon.py
@@ -0,0 +1,11 @@
+import math
+
+# Get input from the user for number of sides and length of each side
+n = int(input("Enter the number of sides: "))
+s = int(input("Enter the length of each side: "))
+
+# Calculate the area of the regular polygon
+area = (n * s ** 2) / (4 * math.tan(math.pi / n))
+
+# Displaying the results as per given number of sides and length of each side
+print(f"Area of the Regular Polygon is {area:.2f}")
