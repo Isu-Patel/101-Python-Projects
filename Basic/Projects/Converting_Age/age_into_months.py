8081c2d521df0093719cbd369b3ab8bb8d632663
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 22:13:24 +0530

    Create age_into_months.py

---
+++ Basic/Projects/Converting_Age/age_into_months.py
@@ -0,0 +1,9 @@
+age_year = int(input("Enter your age in years: "))
+
+months = age_year * 12
+
+print(f"You have lived for approximately {months} months.")
+
+# ---------------------------- Output ---------------------------- #
+# Enter your age in years: 25
+# You have lived for approximately 300 months.
