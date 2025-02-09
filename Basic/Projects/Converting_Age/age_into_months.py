b8e314122d17c9ef7686a916b53fddb8184a0c01
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 22:13:56 +0530

    Update age_into_months.py

--- Basic/Projects/Converting_Age/age_into_months.py
+++ Basic/Projects/Converting_Age/age_into_months.py
@@ -1,7 +1,10 @@
+# Prompt the user to enter their age in years
 age_year = int(input("Enter your age in years: "))
 
+# Convert the age from years to months
 months = age_year * 12
 
+# Display the result to the user
 print(f"You have lived for approximately {months} months.")
 
 # ---------------------------- Output ---------------------------- #
