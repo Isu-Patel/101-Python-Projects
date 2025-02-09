db008bc0adb9e360c590517848c7ad3ae9893522
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 21:57:56 +0530

    Update Feedback_for_something.py

--- Basic/Projects/Feedback_for_something.py
+++ Basic/Projects/Feedback_for_something.py
@@ -1,16 +1,27 @@
 def ask_for_feedback():
 
-  feedback = input("How did you feel this Course? ")
+    feedback = input("How did you find the course? ")
 
-  rating = input("Please rate the course from 1 to 5 ( 1 being poor rating and 5 means excellent rating ): ")
+    rating = input("Please rate the course from 1 to 5(1 bring poor and 5 being excellent): ")
 
-  suggestions = input("Do you have Any suggestions for improvement? ")
+    suggestions = input("Do you have Any suggestions for improvement? ")
 
-  print("\nThanks for your feedback!")
+    print("\nThank you for your feedback!")
 
-  print("\nHere is the feedback you provided: ")
-  print(f"Course Feedback: {feedback}")
-  print(f"Rating: {rating}")
-  print(f"Suggestions: {suggestions}")
+
+    print("\nHere is the feedback you provided: ")
+    print(f"Course Feedback: {feedback}")
+    print(f"Rating: {rating}")
+    print(f"Suggestions: {suggestions}")
 
 ask_for_feedback()
+
+# ------------------------------- Output -------------------------------- #
+# How did you find the course? It was great!
+# Please rate the course from 1 to 5(1 bring poor and 5 being excellent): 5
+# Do you have any suggestions for improvement? No, it was perfect!
+# Thank you for your feedback!
+# Here is the feedback you provided:
+# Course Feedback: It was great!
+# Rating: 5
+# Suggestions: No, it was perfect!
