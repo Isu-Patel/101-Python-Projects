b9160bc93eb1a9cf4ec1f8eeb69b50ce69321b7a
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 22:28:43 +0530

    Update Fibonacci_Sequence.py

--- Basic/Projects/Fibonacci_Sequence.py
+++ Basic/Projects/Fibonacci_Sequence.py
@@ -1,10 +1,17 @@
+# Prompt the user to enter the number of terms for the Fibonacci sequence
 n = int(input("Enter the number of terms:"))
+
+# Initialize the first two terms of the Fibonacci sequence
 f1, f2 = 0, 1
 
+# Print the header for the Fibonacci sequence output
 print("Fibonacci Sequence: ")
 
+# Loop through the range of the number of terms
 for i in range(n):
+    # Print the current term in the sequence
     print(f1, end=" ")
+    # Update the values of f1 and f2 to the next terms in the sequence
     f1, f2 = f2, f1 + f2
 
 # ------------------------- Output --------------------------------
