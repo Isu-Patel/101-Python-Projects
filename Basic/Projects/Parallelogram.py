5ec7f71a8b1626972b189327f1d18563325fafea
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 21:58:34 +0530

    Update Parallelogram.py

--- Basic/Projects/Parallelogram.py
+++ Basic/Projects/Parallelogram.py
@@ -5,5 +5,10 @@ height = float(input("Enter the height of the parallelogram: "))
 # Executing Formula of finding area of parallelogram which is base X Height or b X h
 area = base * height
 
-# Displaying the results as per given base and height
+# Telling what is the area of the base and height given by the user.
 print(f"The area of the parallelogram is {area}")
+
+# ---------------------------- Output ---------------------------- #
+# Enter the base of the parallelogram: 5
+# Enter the height of the parallelogram: 6
+# The area of the parallelogram is 30.0
