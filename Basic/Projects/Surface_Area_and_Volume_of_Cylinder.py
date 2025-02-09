e1a67bac6d9e210972b7a95ba533e4fc1e4ab81d
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 21:59:05 +0530

    Update Surface_Area_and_Volume_of_Cylinder.py

--- Basic/Projects/Surface_Area_and_Volume_of_Cylinder.py
+++ Basic/Projects/Surface_Area_and_Volume_of_Cylinder.py
@@ -8,5 +8,11 @@ volume = math.pi * radius ** 2 * height
 surface_area = 2 * math.pi * radius * (radius + height)
 
 # Displaying the results as per given radius and height
-print("Volume of the Cylinder is: ", round(volume, 2))
-print("Surface Area of the Cylinder is: ", round(surface_area, 2))
+print("Volume of the Cylinder is:", round(volume, 2))
+print("Surface Area of the Cylinder is:", round(surface_area, 2))
+
+# ---------------------------- Output ---------------------------- #
+# Enter the radius of the cylinder: 5
+# Enter the height of the cylinder: 6
+# Volume of the Cylinder is: 471.24
+# Surface Area of the Cylinder is: 251.33
