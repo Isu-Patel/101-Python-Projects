b644842486997115b3a321bb304811d6eee6020d
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 21:57:10 +0530

    Update Area_of_Regular_Polygon.py

--- Basic/Projects/Area_of_Regular_Polygon.py
+++ Basic/Projects/Area_of_Regular_Polygon.py
@@ -9,3 +9,8 @@ area = (n * s ** 2) / (4 * math.tan(math.pi / n))
 
 # Displaying the results as per given number of sides and length of each side
 print(f"Area of the Regular Polygon is {area:.2f}")
+
+# ---------------------------- Output ---------------------------- #
+# Enter the number of sides: 5
+# Enter the length of each side: 6
+# Area of the Regular Polygon is 61.94
