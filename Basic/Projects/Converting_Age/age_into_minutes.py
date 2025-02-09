1977da45739792082571d01baca3972a76a35c20
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-09 22:08:01 +0530

    Create age_into_minutes.py

---
+++ Basic/Projects/Converting_Age/age_into_minutes.py
@@ -0,0 +1,9 @@
+age_year = int(input("Enter your age in years: "))
+
+minutes = age_year * 365 * 24 * 60
+
+print(f"You have lived for approximately {minutes} minutes.")
+
+# ---------------------------- Output ---------------------------- #
+# Enter your age in years: 25
+# You have lived for approximately 1314000 minutes.
