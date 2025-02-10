13f6ed402743dc62c6867fbc9405da183a06c237
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-10 22:27:11 +0530

    Create Reversing_String.py

---
+++ Basic/Projects/Reversing_String.py
@@ -0,0 +1,12 @@
+# Prompt the user to enter a string
+text = input("Enter a string:")
+
+# Reverse the entered string using slicing
+reversed_text = text[::-1]
+
+# Print the reversed string
+print(f"The reversed string is: {reversed_text}")
+
+# ------------------------- Output ------------------------------- #
+# Enter a string: Hello, World!
+# The reversed string is: !dlroW ,olleH
