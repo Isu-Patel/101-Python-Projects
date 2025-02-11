e669f9f32b504dc4605f582539ee05ff766746f3
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-11 23:57:03 +0530

    Create Sorting_of_List.py

---
+++ Basic/Projects/Sorting_of_List.py
@@ -0,0 +1,12 @@
+# Get a list of strings from the user
+strings = input("Enter a list of strings separated by spaces: ").split()
+
+# Sort the list of strings
+strings.sort()
+
+# Print the sorted list
+print("Sorted list:", strings)
+
+# ---------------------------- Output ---------------------------- #
+# Enter a list of strings separated by spaces: red green blue
+# Sorted list: [blue, green, red]
