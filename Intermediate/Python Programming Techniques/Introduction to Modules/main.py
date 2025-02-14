9283cdce69f9651645d9ef2613cacf7a6b6fc062
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-14 21:55:04 +0530

    Create main.py

---
+++ Intermediate/Python Programming Techniques/Introduction to Modules/main.py
@@ -0,0 +1,12 @@
+# Import the sample module which contains the addition function
+import sample
+
+# Call the addition function and pass 10 and 20 as arguments
+# The result is stored in the variable sum
+sum = sample.addition(10, 20)
+
+# Print the result
+print(sum)
+
+# ---------------------------- Output ---------------------------- #
+# 30
