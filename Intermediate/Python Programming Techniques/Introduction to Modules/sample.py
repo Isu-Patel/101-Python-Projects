218f63eb7aeae7593e05b7b409297103b713c0f2
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-02-14 21:54:43 +0530

    Create sample.py

---
+++ Intermediate/Python Programming Techniques/Introduction to Modules/sample.py
@@ -0,0 +1,24 @@
+"""
+This module contains a function for adding two numbers together.
+"""
+
+import time
+import random
+
+
+def addition(num1, num2):
+    """
+    This function takes two numbers as input and returns their sum.
+    
+    Parameters:
+    num1 (int): The first number to add.
+    num2 (int): The second number to add.
+    
+    Returns:
+    int: The sum of num1 and num2.
+    """
+    result = num1 + num2
+    return result
+
+# ------------------------- Output ------------------------------- #
+# 5
