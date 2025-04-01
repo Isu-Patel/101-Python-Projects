b259a06cf2925203a0a5f2e6ab7272e37b2dd721
ISU WORLD <110623091+Isu-Patel@users.noreply.github.com>
2025-04-01 22:26:40 +0530

    Create Copying, Renaming, Deleting, Moving File.py

---
+++ Advanced/Managing File and Directories/Copying, Renaming, Deleting, Moving File.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+
+
+# shutil.copy('Work.txt', 'destination.txt')  # Copy a file
+# shutil.move('Work.txt', 'destination.txt')  # Move a file
+# os.rename('destination.txt', 'Work.txt')  # Rename a file
+# os.remove('Work.txt')  # Delete a file
+
+# Uncomment As Per your Use
